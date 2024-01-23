77e8e7793b6adb1e3f6429aaa03506d0ed79c578
Paul Joel <files.pticlavilca@gmail.com>
2024-01-23 12:12:12 -0500

    add files

--- js/ecategory.js
+++ js/ecategory.js
@@ -1,37 +1,38 @@
 const urlSearchParams = new URLSearchParams(window.location.search);
-async function findById(id){
-    try{
+async function findById(id) {
+    try {
         const response = await fetch(`http://localhost:8080/api/categories/${id}`);
         const data = await response.json();
         document.addEventListener("DOMContentLoaded", loadData(data));
     }
-    catch(error){
+    catch (error) {
         console.log(error)
     }
 }
 findById(urlSearchParams.get("id"));
-function loadData(data){
+function loadData(data) {
+    document.getElementById("iconText").value = data.icon;
     document.getElementById("nameText").value = data.name;
 }
 
 
-async function update(data = {}){
-    console.log("great idea");
-/*    const response = await fetch(`http://localhost:8080/api/categories/${id}`,{
-           method: "PUT",
-           headers : {
-            'Content-Type' : 'application/json'
-           },
-           body : {
-            'id' : '1',
-            'icon' : 'computer',
-            'name' : 'computers'
-           }
+async function update(data = {}) {
+    const response = await fetch(`http://localhost:8080/api/categories/${id}`, {
+        method: "PUT",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: {
+            'id': '1',
+            'icon': 'computer',
+            'name': 'computers'
+        }
     }
 
     );
-
-    */
 }
 
-document.getElementById("submitButton").addEventListener("click", update);
+document.getElementById("submitButton").addEventListener("click",
+    update({
+        "name" : "console"
+    }));
